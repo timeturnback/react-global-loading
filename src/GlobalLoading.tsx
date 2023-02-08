2726723aba3673818427abf5c6017047bbfbd1d5
timeturnback <time.turnback@gmail.com>
2023-02-08 11:03:38 +0700

    init project and simple global loading component

---
+++ src/GlobalLoading.tsx
@@ -0,0 +1,89 @@
+import React, { CSSProperties, FC, ReactElement, useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
+
+type Listener = React.Dispatch<React.SetStateAction<boolean>> | null;
+export let showLoading: Listener = null;
+
+export const show = () => {
+  if (showLoading) {
+    showLoading(true);
+  }
+};
+
+export const hide = () => {
+  if (showLoading) {
+    showLoading(true);
+  }
+};
+
+export const globalLoading = {
+  show,
+  hide
+};
+
+interface GlobalLoadingProps {
+  children: React.ReactNode;
+  WrapperComponent?: (props: any) => ReactElement;
+  backgroundColor?: string;
+  loadingSize?: number;
+  loadingColor?: string;
+  loadingType?:
+    | 'spin'
+    | 'bars'
+    | 'bubbles'
+    | 'cubes'
+    | 'cylon'
+    | 'spin'
+    | 'spinningBubbles'
+    | 'spokes';
+}
+export const GlobalLoading: FC<GlobalLoadingProps> = props => {
+  const {
+    children,
+    WrapperComponent,
+    loadingSize = 50,
+    loadingColor = '#eee',
+    loadingType = 'spin',
+    backgroundColor = 'rgba(0, 0, 0, 0.6)',
+    ...rest
+  } = props || {};
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    showLoading = setLoading;
+  }, []);
+
+  const _renderLoading = () => (
+    <ReactLoading
+      type={loadingType}
+      color={loadingColor}
+      height={loadingSize}
+      width={loadingSize}
+    />
+  );
+
+  const style = {
+    ...$globalLoading,
+    backgroundColor
+  };
+
+  if (!loading) return null;
+  if (WrapperComponent)
+    return <WrapperComponent {...rest}>{children || _renderLoading()}</WrapperComponent>;
+  return <div style={$globalLoading}>{children || _renderLoading()}</div>;
+};
+
+const $globalLoading: CSSProperties = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'rgba(0, 0, 0, 0.6)',
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
